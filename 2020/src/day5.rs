78cf1464702e6ba65872f7472b347683fd755ee9
Jason Miller <contact@jasonmiller.nl>
2020-12-07 18:05:16 +0100

    Change parse seat to use boolean to usize

--- 2020/src/day5.rs
+++ 2020/src/day5.rs
@@ -4,11 +4,7 @@ use itertools::Itertools;
 fn parse_seat(input: &str) -> usize {
     input
         .bytes()
-        .filter_map(|b| match b {
-            b'F' | b'L' => Some(0),
-            b'B' | b'R' => Some(1),
-            _ => None,
-        })
+        .map(|b| matches!(b, b'B' | b'R') as usize)
         .fold(0, |a, b| (a << 1) + b)
 }
 
