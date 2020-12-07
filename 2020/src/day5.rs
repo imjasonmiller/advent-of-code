bbdc203f559ee6ad42543657f853c3f583855ef9
Jason Miller <contact@jasonmiller.nl>
2020-12-07 13:18:24 +0100

    Optimize parse_seat

--- 2020/src/day5.rs
+++ 2020/src/day5.rs
@@ -1,27 +1,21 @@
 use anyhow::Context;
 use itertools::Itertools;
 
-fn parse_seat(input: &str) -> Option<usize> {
-    let mut row = 0;
-    let mut col = 0;
-
-    for byte in input.bytes() {
-        match byte {
-            b'F' => row <<= 1,
-            b'B' => row = (row << 1) + 1,
-            b'L' => col <<= 1,
-            b'R' => col = (col << 1) + 1,
-            _ => return None,
-        }
-    }
-
-    Some(row * 8 + col)
+fn parse_seat(input: &str) -> usize {
+    input
+        .bytes()
+        .filter_map(|b| match b {
+            b'F' | b'L' => Some(0),
+            b'B' | b'R' => Some(1),
+            _ => None,
+        })
+        .fold(0, |a, b| (a << 1) + b)
 }
 
 pub fn part1(input: &str) -> anyhow::Result<usize> {
     input
         .lines()
-        .filter_map(parse_seat)
+        .map(parse_seat)
         .max()
         .context("No solution was found")
 }
@@ -29,7 +23,7 @@ pub fn part1(input: &str) -> anyhow::Result<usize> {
 pub fn part2(input: &str) -> anyhow::Result<usize> {
     input
         .lines()
-        .filter_map(parse_seat)
+        .map(parse_seat)
         .sorted()
         .tuple_windows()
         .find_map(|(a, b)| (a + 1 != b).then_some(a + 1))
